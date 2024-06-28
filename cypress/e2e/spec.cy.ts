954c066c36ea038088c2241b99e9997e7ee7dfa9
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-06-28 11:25:06 +0900

    settings: node-version 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it('should contain "Get started by ~" text', () => {
+    cy.contains("Get started by editing");
+  });
+
+  it('should contain "Docs" heading anchor with correct href prop', () => {
+    cy.contains("Docs")
+      .should("have.attr", "href")
+      .and("include", "https://nextjs.org/docs");
+  });
+});
